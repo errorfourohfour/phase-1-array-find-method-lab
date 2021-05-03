99f0b5e134a53b8dd67a247599e7c88f290682e4
Zach Brookman <zachbrookman48@gmail.com>
2021-05-03 14:43:07 -0400

    commit tree 34c34d10c2803b3325f5a50ce23c1aa66cb6277e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+function superbowlWin(record){
+console.log(record)
+let answer = record.find(function(season){
+    if(season.result ==='W'){
+        return true
+    } else {
+        return undefined
+    }
+})
+    if (answer !== undefined){
+        return answer.year}
+    else {
+            return undefined
+        }
+
+    }
+
